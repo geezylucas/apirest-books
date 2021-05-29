b974754c416c64a78d8e016c8a6d8eaf4d8d97b8
geezylucas <sorec@live.com>
2021-05-29 01:29:03 -0500

    oath2.0 added successfully

--- src/main/java/com/company/books/app/ApirestBooksApplication.java
+++ src/main/java/com/company/books/app/ApirestBooksApplication.java
@@ -1,13 +1,28 @@
 package com.company.books.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class ApirestBooksApplication {
+public class ApirestBooksApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApirestBooksApplication.class, args);
-	}
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
+    public static void main(String[] args) {
+        SpringApplication.run(ApirestBooksApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        String password = "springboot";
+
+        for (int i = 0; i < 3; i++) {
+            String passwordBCrypt = passwordEncoder.encode(password);
+            System.out.println(passwordBCrypt);
+        }
+    }
 }
--- src/main/java/com/company/books/app/auth/ConfigSecurity.java
+++ src/main/java/com/company/books/app/auth/ConfigSecurity.java
@@ -3,8 +3,12 @@ package com.company.books.app.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -17,6 +21,26 @@ public class ConfigSecurity extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/v1/categorias")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean("authenticationManager")
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 
     @Override
